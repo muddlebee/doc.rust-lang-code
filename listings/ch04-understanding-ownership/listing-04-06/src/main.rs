3d1ce0c9e68342ef5ce4f98017dc080193639c49
Anwesh <anweshknayak@gmail.com>
2022-10-03 08:50:14 +0530

    mut function parameters

--- listings/ch04-understanding-ownership/listing-04-06/src/main.rs
+++ listings/ch04-understanding-ownership/listing-04-06/src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
-    let s = String::from("hello");
+    let mut s = String::from("hello");
+    change(&mut s);
+    println!("{}", s);
 
-    change(&s);
 }
 
-fn change(some_string: &String) {
+fn change(some_string: &mut String) {
     some_string.push_str(", world");
 }
